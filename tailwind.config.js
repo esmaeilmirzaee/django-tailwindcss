a18174d3af72d26d3c942632c672e1b99c6633cc
Ishmael MIRZAEE <IshmaelMIRZAEE@gmail.com>
2023-04-11 22:26:15 +0330

    More customization on TailwindCSS.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
+    colors: {
+      black: colors.stone,
+      ...colors,
+    },
     extend: {},
   },
   plugins: [],
